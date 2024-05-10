9736df72c8f8eafa7fe8289927068039a55df5e4
Diego Romero <draketromero@gmail.com>
2024-05-09 22:52:30 -0600

    Unnecesary created files

--- src/status-validation/dto/update-status-validation.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateStatusValidationDto } from './create-status-validation.dto';
-
-export class UpdateStatusValidationDto extends PartialType(CreateStatusValidationDto) {}
--- src/status-validation/status-validation.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StatusValidationService } from './status-validation.service';
-import { StatusValidationController } from './status-validation.controller';
-
-@Module({
-  controllers: [StatusValidationController],
-  providers: [StatusValidationService],
-})
-export class StatusValidationModule {}
