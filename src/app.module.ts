3cab085e4ecbb6374e14fe98780ddb76153f87f9
Diego Romero <draketromero@gmail.com>
2024-05-10 01:11:13 -0600

    Updated modules for regroup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,14 @@ import { AuthModule } from './auth/auth.module';
 import { DefaultModule } from './default/default.module';
 import { UserModule } from './user/user.module';
 import { SeedModule } from './seed/seed.module';
-import { PositionModule } from './position/position.module';
-import { HierarchyModule } from './hierarchy/hierarchy.module';
-import { ValidationFormModule } from './validation-form/validation-form.module';
-import { ProfileModule } from './profile/profile.module';
-import { PhotographModule } from './photograph/photograph.module';
 import { HeadquarterModule } from './headquarter/headquarter.module';
 import { MembershipModule } from './membership/membership.module';
-import { MembershipOfferModule } from './membership-offer/membership-offer.module';
-import { StatusValidationModule } from './status-validation/status-validation.module';
+import { OccupancyModule } from './occupancy/occupancy.module';
+import { ValidationModule } from './validation/validation.module';
 
 @Module({
-  imports: [AuthModule, DefaultModule, UserModule, SeedModule, PositionModule, HierarchyModule, ValidationFormModule, ProfileModule, PhotographModule, HeadquarterModule, MembershipModule, MembershipOfferModule, StatusValidationModule],
+  imports: [AuthModule, DefaultModule, UserModule, SeedModule, HeadquarterModule, MembershipModule, OccupancyModule, ValidationModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
