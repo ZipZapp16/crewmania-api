11f8a4c6c0263132f9f9f2ae1a568f90770b2e5f
Diego Romero <draketromero@gmail.com>
2024-05-16 14:56:44 -0600

    Imported Prisma module

--- src/membership/membership.module.ts
+++ src/membership/membership.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MembershipService } from './membership.service';
 import { MembershipController } from './membership.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [MembershipController],
   providers: [MembershipService],
+  imports: [PrismaModule]
 })
 export class MembershipModule {}
