a617fcb4206b7cf34fc50b84cba9275fb2157559
Diego Romero <draketromero@gmail.com>
2024-05-18 23:48:05 -0600

    Imported Prisma Module on modules files

--- src/occupancy/occupancy.module.ts
+++ src/occupancy/occupancy.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OccupancyService } from './occupancy.service';
 import { OccupancyController } from './occupancy.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [OccupancyController],
   providers: [OccupancyService],
+  imports: [PrismaModule]
 })
 export class OccupancyModule {}
--- src/validation/validation.module.ts
+++ src/validation/validation.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ValidationService } from './validation.service';
 import { ValidationController } from './validation.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [ValidationController],
   providers: [ValidationService],
+  imports: [PrismaModule]
 })
 export class ValidationModule {}
