7026749fe4ff01228ed69aef1f27993821da7279
Diego Romero <draketromero@gmail.com>
2024-05-21 00:10:31 -0600

    Imported Membership module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { MembershipModule } from 'src/membership/membership.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [PrismaModule]
+  imports: [PrismaModule, MembershipModule]
 })
 export class UserModule { }
