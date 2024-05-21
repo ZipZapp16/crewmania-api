24ed36bb757529b80a8ade18c5f5ababb7a9258c
Diego Romero <draketromero@gmail.com>
2024-05-21 00:12:39 -0600

    Exported Membreship service

--- src/membership/membership.module.ts
+++ src/membership/membership.module.ts
@@ -4,6 +4,7 @@ import { MembershipController } from './membership.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  exports: [MembershipService],
   controllers: [MembershipController],
   providers: [MembershipService],
   imports: [PrismaModule]
