07e2903daf49f5f69e14106e8e328d042872e007
Diego Romero <draketromero@gmail.com>
2024-05-27 16:36:24 -0600

    Imported modules for insert seed data methods

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { OccupancyModule } from 'src/occupancy/occupancy.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ValidationModule } from 'src/validation/validation.module';
+import { MembershipModule } from 'src/membership/membership.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [PrismaModule, OccupancyModule, ValidationModule, MembershipModule]
 })
 export class SeedModule {}
