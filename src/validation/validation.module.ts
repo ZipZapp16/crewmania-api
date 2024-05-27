f92261ff47d06a1a26f841cd4f5d497d79baac5c
Diego Romero <draketromero@gmail.com>
2024-05-27 12:38:29 -0600

    Exported service file

--- src/validation/validation.module.ts
+++ src/validation/validation.module.ts
@@ -4,6 +4,7 @@ import { ValidationController } from './validation.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  exports: [ValidationService],
   controllers: [ValidationController],
   providers: [ValidationService],
   imports: [PrismaModule]
