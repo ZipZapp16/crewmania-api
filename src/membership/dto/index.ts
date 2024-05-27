4b9e6163cb5632b46b727ce8f80ac672f91b1bac
Diego Romero <draketromero@gmail.com>
2024-05-27 16:26:45 -0600

    Added importation of file on dto

--- src/membership/dto/index.ts
+++ src/membership/dto/index.ts
@@ -1,4 +1,6 @@
+export { CreateOfferDto } from "./create-offer.dto";
+export { UpdateOfferDto } from "./update-offer.dto";
 export { CreateMembershipDto } from "./create-membership.dto";
-export { CreateMembershipOfferDto } from "./create-membership-offer.dto";
 export { UpdateMembershipDto } from "./update-membership.dto";
+export { CreateMembershipOfferDto } from "./create-membership-offer.dto";
 export { UpdateMembershipOfferDto } from "./update-membership-offer.dto";
