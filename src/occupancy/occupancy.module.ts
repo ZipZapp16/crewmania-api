d095bee8c8915a67e2a6c0006571594449ae8f8c
Diego Romero <draketromero@gmail.com>
2024-05-27 12:36:10 -0600

    Imported PrismaModule an exported service file

--- src/occupancy/occupancy.module.ts
+++ src/occupancy/occupancy.module.ts
@@ -6,6 +6,7 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
   controllers: [OccupancyController],
   providers: [OccupancyService],
-  imports: [PrismaModule]
+  imports: [PrismaModule],
+  exports: [OccupancyService]
 })
 export class OccupancyModule {}
