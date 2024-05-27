2f6b34c1774affe298d2031759e33dbd4f4a139d
Diego Romero <draketromero@gmail.com>
2024-05-27 16:25:54 -0600

    Added extention on importation dto

--- src/user/dto/index.ts
+++ src/user/dto/index.ts
@@ -1,7 +1,7 @@
 export { CreateUserDto } from "./create-user.dto";
 export { UpdateUserDto } from "./update-user.dto";
 
-export { CreateUserValidationDto } from "./create-user-validation";
+export { CreateUserValidationDto } from "./create-user-validation.dto";
 export { UpdateUserValidationDto } from "./update-user-validation.dto";
 
 export { CreateUserOccupancyDto } from './create-user-occupancy.dto';
