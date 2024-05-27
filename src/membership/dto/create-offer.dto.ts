e8f4cd0de0764188c086c6b909798407e688cf52
Diego Romero <draketromero@gmail.com>
2024-05-27 16:28:14 -0600

    Created files to create and update  dto

---
+++ src/membership/dto/create-offer.dto.ts
@@ -0,0 +1,24 @@
+import { Type } from "class-transformer";
+import { IsBoolean, IsDate, IsNumber, IsString, MinLength } from "class-validator";
+
+export class CreateOfferDto {
+
+    @MinLength(1)
+    @IsString()
+    name: string;
+
+    @IsNumber()
+    @Type(() => Number)
+    percentageOffer: number;
+
+    @IsDate()
+    @Type(() => Date)
+    dateStart: Date;
+
+    @IsDate()
+    @Type(() => Date)
+    dateEnd: Date;
+
+    @IsBoolean()
+    enabled: boolean;
+}
