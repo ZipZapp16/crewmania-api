4d749e51588a11537e3db2aeeafbb127b8fe6a49
Diego Romero <draketromero@gmail.com>
2024-05-28 20:34:14 -0600

    Removed Headquarter module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { AuthModule } from './auth/auth.module';
 import { DefaultModule } from './default/default.module';
 import { UserModule } from './user/user.module';
 import { SeedModule } from './seed/seed.module';
-import { HeadquarterModule } from './headquarter/headquarter.module';
 import { MembershipModule } from './membership/membership.module';
 import { OccupancyModule } from './occupancy/occupancy.module';
 import { ValidationModule } from './validation/validation.module';
 
 @Module({
-  imports: [AuthModule, DefaultModule, UserModule, SeedModule, HeadquarterModule, MembershipModule, OccupancyModule, ValidationModule],
+  imports: [AuthModule, DefaultModule, UserModule, SeedModule, MembershipModule, OccupancyModule, ValidationModule],
   controllers: [],
   providers: [],
 })
