b833f7dc1fafe5a97fe1cee0d67db81896c486d5
Diego Romero <draketromero@gmail.com>
2024-06-11 19:33:09 -0600

    Added missing interfaces exportations on index file

--- src/user/interfaces/index.ts
+++ src/user/interfaces/index.ts
@@ -1,2 +1,4 @@
 export { UserResponse } from "./user-response.interface";
 export { UserValidationResponse } from "./user-validation-response.interface";
+export { UserMembershipResponse } from './user-membership-response.interface';
+export { UserOccupancyResponse } from "./user-occupancy-response.interface";
