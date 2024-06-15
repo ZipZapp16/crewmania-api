9cffceef402d613cd43773ccaee8724ef15fbbd8
Diego Romero <draketromero@gmail.com>
2024-06-14 23:39:53 -0600

    Fixed exportation on index file

--- src/validation/dto/index.ts
+++ src/validation/dto/index.ts
@@ -1,4 +1,4 @@
 export { CreateStatusValidationDto } from "./create-status-validation.dto";
 export { CreateValidationFormDto } from "./create-validation-form.dto";
 export { UpdateStatusValidationDto } from "./update-status-validation.dto";
-export { UpdateValidationDto } from "./update-validation-form.dto";
+export { UpdateValidationFormDto } from "./update-validation-form.dto";
---
+++ src/validation/interfaces/index.ts
@@ -0,0 +1,2 @@
+export { StatusValidationResponse } from "./status-validation-response.interface";
+export { ValidationResponse } from "./validation-response.interface";
