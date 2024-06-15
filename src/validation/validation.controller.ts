8a4c018a5676f2f54ddb3e91fda05e97de75de2c
Diego Romero <draketromero@gmail.com>
2024-06-14 23:35:32 -0600

    Added missing rest methods on validation module

--- src/validation/validation.controller.ts
+++ src/validation/validation.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { ValidationService } from './validation.service';
 import { CreateValidationFormDto } from './dto/create-validation-form.dto';
 import { CreateStatusValidationDto } from './dto/create-status-validation.dto';
 import { ValidationResponse } from './interfaces/validation-response.interface';
 import { StatusValidationResponse } from './interfaces/status-validation-response.interface';
 import { ApiTags } from '@nestjs/swagger';
+import { UpdateValidationFormDto } from './dto/update-validation-form.dto';
+import { UpdateStatusValidationDto } from './dto';
 
 @ApiTags('Validation')
 @Controller('validation')
 export class ValidationController {
-  constructor(private readonly validationService: ValidationService) {}
+  constructor(private readonly validationService: ValidationService) { }
 
   // * Comienzan endpoints para validationForms
   @Post('/forms')
@@ -22,6 +24,24 @@ export class ValidationController {
     return this.validationService.findAllValidationForms();
   }
 
+  @Get('/form/:formId')
+  findValidationForm(@Param('formId', ParseUUIDPipe) formId: string): Promise<ValidationResponse> {
+    return this.validationService.findValidationForm(formId);
+  }
+
+  @Patch('/form/:formId')
+  updateValidationForm(
+    @Param('formId', ParseUUIDPipe) formId: string,
+    @Body() updateValidationFormDto: UpdateValidationFormDto
+  ): Promise<ValidationResponse> {
+    return this.validationService.updateValidationForm(formId, updateValidationFormDto);
+  }
+
+  @Delete('/form/:formId')
+  deleteValidationForm(@Param('formId', ParseUUIDPipe) formId: string): Promise<ValidationResponse> {
+    return this.validationService.deleteValidationForm(formId);
+  }
+
   // * Comienzan endpoints para statusValidations
   @Post('/status')
   createStatusValidation(@Body() createStatusValidationDto: CreateStatusValidationDto): Promise<StatusValidationResponse> {
@@ -31,5 +51,23 @@ export class ValidationController {
   @Get('/status')
   findAllStatusValidation(): Promise<StatusValidationResponse> {
     return this.validationService.findAllStatusValidation();
+  }
+
+  @Get('/status/:statusId')
+  findStatusValidation(@Param('statusId', ParseUUIDPipe) statusId: string): Promise<StatusValidationResponse> {
+    return this.validationService.findStatusValidation(statusId);
+  }
+
+  @Patch('/status/:statusId')
+  updateStatusValidation(
+    @Param('statusId', ParseUUIDPipe) statusId: string,
+    @Body() updateStatusValidationDto: UpdateStatusValidationDto
+  ): Promise<StatusValidationResponse> {
+    return this.validationService.updateStatusValidation(statusId, updateStatusValidationDto);
+  }
+
+  @Delete('/status/:statusId')
+  deleteStatusValidation(@Param('statusId', ParseUUIDPipe) statusId: string): Promise<StatusValidationResponse> {
+    return this.validationService.deleteStatusValidation(statusId);
   }
 }
--- src/validation/validation.service.ts
+++ src/validation/validation.service.ts
@@ -1,10 +1,11 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateValidationFormDto } from './dto/create-validation-form.dto';
-import { UpdateValidationDto } from './dto/update-validation-form.dto';
+import { UpdateValidationFormDto } from './dto/update-validation-form.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateStatusValidationDto } from './dto/create-status-validation.dto';
 import { ValidationResponse } from './interfaces/validation-response.interface';
 import { StatusValidationResponse } from './interfaces/status-validation-response.interface';
+import { UpdateStatusValidationDto } from './dto/update-status-validation.dto';
 
 @Injectable()
 export class ValidationService {
@@ -20,8 +21,7 @@ export class ValidationService {
         data: validations
       };
     } catch (error) {
-      console.log(error)
-      throw new BadRequestException(`Error al crear la validacion ${error}`);
+      throw new BadRequestException(`Error to create the validation ${error}`);
     }
   }
 
@@ -34,10 +34,55 @@ export class ValidationService {
         message: "success",
         data: validations
       };
-      
+
     } catch (error) {
-      console.log(error)
-      throw new NotFoundException(`No se encontraron tipos de validaciones.`);
+      throw new NotFoundException(`There isn't validations registered.`);
+    }
+  }
+
+  async findValidationForm(formId: string): Promise<ValidationResponse> {
+    try {
+      const validation = await this.prismaService.validationForm.findUnique({ where: { id: formId } });
+
+      return {
+        status: "ok",
+        message: "success",
+        data: validation
+      };
+    } catch (error) {
+      throw new NotFoundException(`Validation form with id ${formId} doesn't exists.`)
+    }
+  }
+
+  async updateValidationForm(formId: string, updateValidationFormDto: UpdateValidationFormDto): Promise<ValidationResponse> {
+    try {
+      const { data: validationFormToUpdate } = await this.findValidationForm(formId);
+
+      const validationFormUpdated = await this.prismaService.validationForm.update({ where: { id: validationFormToUpdate['id'] }, data: updateValidationFormDto });
+
+      return {
+        status: "ok",
+        message: "success",
+        data: validationFormUpdated
+      };
+    } catch (error) {
+      throw new BadRequestException(`Error to update the validation with id ${formId}. ${error}`);
+    }
+  }
+
+  async deleteValidationForm(formId: string): Promise<ValidationResponse> {
+    try {
+      const { data: validationFormToDelete } = await this.findValidationForm(formId);
+
+      const validationFormDeleted = await this.prismaService.validationForm.delete({ where: { id: validationFormToDelete['id'] }});
+
+      return {
+        status: "ok",
+        message: "success",
+        data: validationFormDeleted
+      };
+    } catch (error) {
+      throw new BadRequestException(`Error to update the validation with id ${formId}. ${error}`);
     }
   }
 
@@ -52,23 +97,67 @@ export class ValidationService {
         data: status
       };
     } catch (error) {
-      console.log(error)
-      throw new BadRequestException(`Error al crear la validacion ${error}`);
+      throw new BadRequestException(`Error to create the validation ${error}`);
     }
   }
 
   async findAllStatusValidation(): Promise<StatusValidationResponse> {
     try {
-      const status = await this.prismaService.statusValidation.findMany();
+      const statusValidations = await this.prismaService.statusValidation.findMany();
 
       return {
         status: "ok",
         message: "success",
-        data: status
+        data: statusValidations
       };
     } catch (error) {
-      console.log(error)
       throw new NotFoundException(`No se encontraron tipos de validaciones.`);
+    }
+  }
+
+  async findStatusValidation(statusValidationId: string): Promise<StatusValidationResponse> {
+    try {
+      const statusValidation = await this.prismaService.statusValidation.findUnique({ where: { id: statusValidationId }});
+
+      return {
+        status: "ok",
+        message: "success",
+        data: statusValidation
+      };
+    } catch (error) {
+      throw new NotFoundException(`Status valiation with id ${statusValidationId} doesn't exist. ${error}`);
+    }
+  }
+
+  async updateStatusValidation(statusValidationId: string, updateStatusValidationDto: UpdateStatusValidationDto): Promise<StatusValidationResponse> {
+    try {
+      const { data: statusValidationToUpdate } = await this.findStatusValidation(statusValidationId);
+
+      const statusValidationUpdated = await this.prismaService.statusValidation.update({ where: { id: statusValidationToUpdate['id'] }, data: updateStatusValidationDto });
+
+      return {
+        status: "ok",
+        message: "success",
+        data: statusValidationUpdated
+      };
+    } catch (error) {
+      throw new BadRequestException(`Error to update the validation with id ${statusValidationId} ${error}`);
+    }
+  }
+
+  async deleteStatusValidation(statusValidationId: string): Promise<StatusValidationResponse> {
+    try {
+      const { data: statusValidationToDelete } = await this.findStatusValidation(statusValidationId);
+
+      const statusValidationDeleted = await this.prismaService.statusValidation.delete({ where: { id: statusValidationToDelete['id'] } });
+
+      return {
+        status: "ok",
+        message: "success",
+        data: statusValidationDeleted
+      };
+    } catch (error) {
+      throw new BadRequestException(`Error to delete the validation with id ${statusValidationId} ${error}`);
     }
   }
 }
