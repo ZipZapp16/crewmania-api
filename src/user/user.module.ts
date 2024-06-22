cb6ef3e7bbef03fa8a120f968211607f92894983
Diego Romero <draketromero@gmail.com>
2024-06-21 22:38:45 -0600

    Common module imported on user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { SubscriptionModule } from 'src/subscription/subscription.module';
+import { CommonModule } from 'src/common/common.module';
 
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [PrismaModule, SubscriptionModule]
+  imports: [PrismaModule, SubscriptionModule, CommonModule]
 })
 export class UserModule { }
