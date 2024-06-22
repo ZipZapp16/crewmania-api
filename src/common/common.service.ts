50477af56109eba932668e9a478952fe109a75ce
Diego Romero <draketromero@gmail.com>
2024-06-21 22:22:53 -0600

    Method to upload to S3 image validation of user account addded

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { PutObjectCommand, PutObjectCommandInput, PutObjectCommandOutput, S3Client } from '@aws-sdk/client-s3';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CommonService {
-  create() {
-    return 'This action adds a new common';
+
+  private readonly client: S3Client = new S3Client({
+    region: process.env.AWS_BUCKET_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_SECRET_KEY,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+  });
+
+  async uploadFileS3(file: Express.Multer.File, userId: string, folder: string): Promise<PutObjectCommandOutput> {
+    if (!file) {
+      throw new BadRequestException(`The file that you have selected does not comply with the following formats: ${process.env.AWS_FORMAT_IMAGES}`);
+    }
+
+    const uploadParams: PutObjectCommandInput = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `${userId}/${folder}_${userId}.jpg`,
+      Body: file.buffer
+    };
+
+    const command = new PutObjectCommand(uploadParams);
+    return await this.client.send(command);
   }
 }
