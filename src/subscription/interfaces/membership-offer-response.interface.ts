25cad95ae0cdfb8e63c08fb8d2328043d6729a77
Diego Romero <draketromero@gmail.com>
2024-06-28 21:11:53 -0600

    Updated interface response

--- src/subscription/interfaces/membership-offer-response.interface.ts
+++ src/subscription/interfaces/membership-offer-response.interface.ts
@@ -1,16 +1,9 @@
-import { Membership, MembershipOffer, Offer } from "@prisma/client";
+import { MembershipOffer, Offer } from "@prisma/client";
 import { DataResponse } from "src/common/interfaces";
 
 export interface MembershipOfferResponse extends DataResponse {
     data: MembershipOffer | MembershipOffer[];
 }
-
-interface findMO {
-    id: string;
-    membership: string | Membership[];
-    offer: string | Offer[];
-}
-
 export interface FindMembershipOfferResponse extends DataResponse {
-    data: findMO | findMO[];
+    data: Offer | Offer[];
 }
