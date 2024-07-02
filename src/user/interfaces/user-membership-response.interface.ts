09a32ba37c6582fad6375101e1068d5cf417134a
Diego Romero <draketromero@gmail.com>
2024-07-02 00:03:33 -0600

    Created interface UserSubscription

--- src/user/interfaces/user-membership-response.interface.ts
+++ src/user/interfaces/user-membership-response.interface.ts
@@ -1,6 +1,11 @@
 import { UserMembership } from "@prisma/client";
 import { DataResponse } from "src/common/interfaces";
 
+export interface UserSubscription { 
+    membershipId: string;
+    daysToRenew: number;
+    type: string;
+};
 export interface UserMembershipResponse extends DataResponse {
-    data: UserMembership | UserMembership[];
+    data: UserMembership | UserMembership[] | UserSubscription | UserSubscription[];
 }
