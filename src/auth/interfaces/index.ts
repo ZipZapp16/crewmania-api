8925a2085d493e66dfd2c269bc732b29f059f59c
Diego Romero <draketromero@gmail.com>
2024-07-05 21:07:36 -0600

    Barrel files added to export the folder files

--- src/auth/interfaces/index.ts
+++ src/auth/interfaces/index.ts
@@ -1,2 +1,3 @@
-export { JwtPayload } from "./jwt-payload";
 export { AuthResponse } from "./auth-response.interface";
+export { JwtPayload } from "./jwt-payload.interface";
+export { ValidRoles } from "./valid-roles.interface";
