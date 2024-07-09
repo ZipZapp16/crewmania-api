25eb61199908adf09441ce06d76d3216923ac649
Diego Romero <draketromero@gmail.com>
2024-07-09 16:35:20 -0600

    Created REST methods to log service

--- src/logs/logs.service.ts
+++ src/logs/logs.service.ts
@@ -1,26 +1,85 @@
-import { Injectable } from '@nestjs/common';
-import { CreateLogDto } from './dto/create-log.dto';
-import { UpdateLogDto } from './dto/update-log.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateLogDto, UpdateLogDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { LogsResponse } from './interfaces';
 
 @Injectable()
 export class LogsService {
-  create(createLogDto: CreateLogDto) {
-    return 'This action adds a new log';
+  constructor(
+    private readonly prismaService: PrismaService
+  ) { }
+
+  async create(createLogDto: CreateLogDto): Promise<LogsResponse> {
+    try {
+      const log = await this.prismaService.log.create({ data: createLogDto });
+
+      return {
+        status: "ok",
+        message: "success",
+        data: log
+      };
+    } catch (error) {
+      throw new BadRequestException(`Error to create log. ${error}`);
+    };
   }
 
-  findAll() {
-    return `This action returns all logs`;
+  async findAll(): Promise<LogsResponse> {
+    try {
+      const logs = await this.prismaService.log.findMany();
+
+      return {
+        status: "ok",
+        message: "success",
+        data: logs
+      };
+    } catch (error) {
+      throw new BadRequestException(`Error to find logs. ${error}`);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} log`;
+  async findOne(logId: string): Promise<LogsResponse> {
+    try {
+      const log = await this.prismaService.log.findUnique({ where: { id: logId }});
+
+      return {
+        status: "ok",
+        message: "success",
+        data: log
+      };
+    } catch (error) {
+      throw new BadRequestException(`Error to find log with id ${logId}. ${error}`);
+    }
   }
 
-  update(id: number, updateLogDto: UpdateLogDto) {
-    return `This action updates a #${id} log`;
+  async update(logId: string, updateLogDto: UpdateLogDto): Promise<LogsResponse> {
+    try {
+      const { data: logToUpdate } = await this.findOne(logId);
+
+      const logUpdated = await this.prismaService.log.update({ where: { id: logToUpdate['id'] }, data: updateLogDto });
+
+      return {
+        status: "ok",
+        message: "success",
+        data: logUpdated
+      };
+    } catch (error) {
+      throw new BadRequestException(`Error to update log with id ${logId}. ${error}`);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} log`;
+  async remove(logId: string): Promise<LogsResponse> {
+    try {
+      const { data: logToDelete } = await this.findOne(logId);
+
+      const logDeleted = await this.prismaService.log.delete({ where: { id: logToDelete['id'] } });
+
+      return {
+        status: "ok",
+        message: "success",
+        data: logDeleted
+      };
+    } catch (error) {
+      throw new BadRequestException(`Error to delete log with id ${logId}. ${error}`);
+    }
   }
 }
