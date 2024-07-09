37ab2b0a75bb9d89d0ad1e88c72d2be4d4d27503
Diego Romero <draketromero@gmail.com>
2024-07-09 16:36:10 -0600

    Created log module file

--- src/logs/logs.module.ts
+++ src/logs/logs.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LogsService } from './logs.service';
 import { LogsController } from './logs.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [LogsController],
   providers: [LogsService],
+  exports: [LogsService],
+  imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
 })
 export class LogsModule {}
