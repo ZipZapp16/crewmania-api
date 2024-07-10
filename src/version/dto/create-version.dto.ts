dc2dccf085549652063cfc5fc1d6ea2fdad670a7
Diego Romero <draketromero@gmail.com>
2024-07-09 18:59:30 -0600

    Created Version DTO

--- src/version/dto/create-version.dto.ts
+++ src/version/dto/create-version.dto.ts
@@ -1,1 +1,11 @@
-export class CreateVersionDto {}
+import { IsString, MinLength } from "class-validator";
+
+export class CreateVersionDto {
+    
+    @IsString()
+    number: string;
+
+    @IsString()
+    @MinLength(1)
+    description: string;
+}
