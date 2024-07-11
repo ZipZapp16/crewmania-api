d39e8f309165c0e840ab144a9ae9c483897ab58d
Diego Romero <draketromero@gmail.com>
2024-07-11 00:16:41 -0600

    Added swagger description of fields on DTO

--- src/logs/dto/create-log.dto.ts
+++ src/logs/dto/create-log.dto.ts
@@ -1,7 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, MinLength } from "class-validator";
 
 export class CreateLogDto {
 
+    @ApiProperty({ 
+        default: 'A new user registered', description: 'Description of the log saved'
+    })
     @IsString()
     @MinLength(1)
     description: string;
--- src/version/dto/create-version.dto.ts
+++ src/version/dto/create-version.dto.ts
@@ -1,10 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString, MinLength } from "class-validator";
 
 export class CreateVersionDto {
     
+    @ApiProperty({ 
+        default: '1.0.0', description: 'Number of the version that is available to the type of platform'
+    })
     @IsString()
     number: string;
 
+    @ApiProperty({ 
+        default: 'Operations start', description: 'Description of the number version saved'
+    })
     @IsString()
     @MinLength(1)
     description: string;
