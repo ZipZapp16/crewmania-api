2c4c01aaa4ca56d4956810e68516202a2d32b357
Diego Romero <draketromero@gmail.com>
2024-07-15 22:56:44 -0600

    Implementation of Auth decorators for JWT verification on version REST methods

--- src/version/version.controller.ts
+++ src/version/version.controller.ts
@@ -13,26 +13,31 @@ export class VersionController {
   constructor(private readonly versionService: VersionService) {}
 
   @Post('/create')
+  @Auth(ValidRoles.admin)
   create(@Body() createVersionDto: CreateVersionDto): Promise<VersionResponse> {
     return this.versionService.create(createVersionDto);
   }
 
   @Get('/findAll')
+  @Auth(ValidRoles.admin, ValidRoles.user)
   findAll(): Promise<VersionResponse> {
     return this.versionService.findAll();
   }
 
   @Get('/:versionId/findOne')
+  @Auth(ValidRoles.admin, ValidRoles.user)
   findOne(@Param('versionId', ParseUUIDPipe) versionId: string): Promise<VersionResponse> {
     return this.versionService.findOne(versionId);
   }
 
   @Patch('/:versionId/update')
+  @Auth(ValidRoles.admin)
   update(@Param('versionId', ParseUUIDPipe) versionId: string, @Body() updateVersionDto: UpdateVersionDto): Promise<VersionResponse> {
     return this.versionService.update(versionId, updateVersionDto);
   }
 
   @Delete('/:versionId/delete')
+  @Auth(ValidRoles.admin)
   remove(@Param('versionId', ParseUUIDPipe) versionId: string): Promise<VersionResponse> {
     return this.versionService.remove(versionId);
   }
